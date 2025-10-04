0987127cb2dd2b459aa557b76162ad2e8b11df44
Hopelesszz <hopelesssssz@gmail.com>
2025-10-04 15:19:29 +0300

    Update Account_info.jsx

--- src/pages/Account_info/Account_info.jsx
+++ src/pages/Account_info/Account_info.jsx
@@ -89,7 +89,7 @@ function Account_info () {
                     <div>
                         {awards.map((award) => {
                             const userAward = userAwards.find((ua) => ua.achievementId === award._id);
-                            console.log(userAward.progress);
+                            console.log(userAward);
                             const progress = userAward.progress;
                             const status = userAward.completed ? "Completed" : "In progress";
                             const percent = Math.min((progress / award.target) * 100, 100);
