721e0f1623e6b29c302248baded03c72f1cc82f4
Egor <savitsky.egor318@gmail.com>
2025-10-05 14:45:01 +0300

    Error fixing

--- src/Protected_route.jsx
+++ src/Protected_route.jsx
@@ -1,44 +1,12 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "./context/AuthContext";
-import axios from "axios";
+import { AuthContext } from "../src/context/AuthContext.jsx";
 
 const ProtectedRoute = ({ children }) => {
-  const { user, dispatch } = useContext(AuthContext);
-  const [loading, setLoading] = useState(true);
-  const API_URL = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    const checkToken = async () => {
-      try {
-        const res = await axios.get(`${API_URL}/auth/checkToken`, {
-          withCredentials: true,
-        });
-
-        if (res.data.status === false) {
-          dispatch({ type: "LOGOUT" });
-          localStorage.removeItem("user");
-        }
-      } catch (err) {
-        console.log("Token check error:", err);
-        dispatch({ type: "LOGOUT" });
-        localStorage.removeItem("user");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    checkToken();
-  }, [dispatch, API_URL]);
-
-  if (loading) {
-    return <div>Loading...</div>; 
-  }
-
+  const { user } = useContext(AuthContext);
   if (!user) {
-    return <Navigate to="/auth" />;
+    return <Navigate to="/auth" replace />;
   }
-
   return children;
 };
 
